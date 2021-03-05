dc94e74b704a129e54e61dcdcc7b20b97c811ece
EbadSyed <syedebadurrahman@gmail.com>
2021-03-05 05:20:02 -0500

    numpy learn

--- readData.py
+++ readData.py
@@ -3,6 +3,8 @@ import serial
 ser = serial.Serial('/dev/ttyACM0')
 ser.baudrate=115200
 
+tanTheta = 0.4434
+
 while True:
     ser_bytes = ser.readline()
     ser_string = ser_bytes.decode("utf-8")
@@ -10,23 +12,36 @@ while True:
 
     if int(ser_list[0]) == 0:
         x1 = int(ser_list[1])
+        if x1 > 113:
+            x1 = 113
     else:
-        x1 = 120
+        x1 = 113
 
     if ser_list[2] == '0':
         x2 = int(ser_list[3])
+        if x2 > 113:
+            x2 = 113
     else:
-        x2 = 120
+        x2 = 113
 
     if ser_list[4] == '0':
         x3 = int(ser_list[5])
+        if x3 > 113:
+            x3 = 113
     else:
-        x3 = 120
+        x3 = 113
 
     if ser_list[6] == '0':
         x4 = int(ser_list[7])
+        if x4 > 113:
+            x4 = 113
     else:
-        x4 = 120
+        x4 = 113
+
+    y1 = int(x1 * tanTheta)
+    y2 = int(x2 * tanTheta)
+    y3 = int(x3 * tanTheta)
+    y4 = int(x4 * tanTheta)
 
     print(x1,x2,x3,x4)
-
+    print(y1,y2,y3,y4)
