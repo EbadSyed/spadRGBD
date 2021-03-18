4343aee1ad9a345052f7dedf9608843f252831a5
EbadSyed <syedebadurrahman@gmail.com>
2021-03-18 03:36:04 -0400

    roi-anim

--- readVL53.py
+++ readVL53.py
@@ -4,19 +4,11 @@ import signal
 
 import VL53L1X
 
-
-print("""distance.py
-Display the distance read from the sensor.
-Uses the "Short Range" timing budget by default.
-Press Ctrl+C to exit.
-""")
-
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
 # Open and start the VL53L1X sensor.
-# If you've previously used change-address.py then you
-# should use the new i2c address here.
-# If you're using a software i2c bus (ie: HyperPixel4) then
-# you should `ls /dev/i2c-*` and use the relevant bus number.
 tof = VL53L1X.VL53L1X(i2c_bus=1, i2c_address=0x29)
 tof.open()
 
@@ -36,59 +28,33 @@ signal.signal(signal.SIGINT, exit_handler)
 
 roi = []
 
-for x in range(5):
-    for y in range(5):
-        roi.append(VL53L1X.VL53L1xUserRoi((3*x), (15 - 3 * y), (3 * x + 3), (15 - 3 * y - 3)))
-        print(3*x)
-        print(15 - 3 * y)
-        print((3 * x + 3))
-        print(15 - 3 * y - 3)
-        print("----")
+for x1 in range(5):
+    for y1 in range(5):
+        roi.append(VL53L1X.VL53L1xUserRoi((3 * x1), (15 - 3 * y1), (3 * x1 + 3), (15 - 3 * y1 - 3)))
 
-print(len(roi))
-print(roi[4])
+fig = plt.figure()
+data = np.zeros((5, 5))
+im = plt.imshow(data, cmap='magma', vmin=0, vmax=500)
+plt.colorbar(fraction=0.1, pad=0.04)
 
 
-while running:
+def init():
+    im.set_data(np.zeros((8, 8)))
 
 
+def animate(iter):
 
-    tof.set_user_roi(VL53L1X.VL53L1xUserRoi(0, 0, 3, 3))
-    tof.start_ranging(1)
-    distance_in_mm1 = tof.get_distance()
-    tof.stop_ranging()
+    i = 0
 
-    tof.set_user_roi(VL53L1X.VL53L1xUserRoi(4, 0, 7, 3))
-    tof.start_ranging(1)
-    distance_in_mm2 = tof.get_distance()
-    tof.stop_ranging()
+    for x in range(5):
+        for y in range(5):
+            tof.set_user_roi(roi[i])
+            tof.start_ranging(1)
+            data[x,y] = tof.get_distance()
+            tof.stop_ranging()
 
-    tof.set_user_roi(VL53L1X.VL53L1xUserRoi(8, 0, 11, 3))
-    tof.start_ranging(1)
-    distance_in_mm3 = tof.get_distance()
-    tof.stop_ranging()
+    im.set_data(data)
 
-    tof.set_user_roi(VL53L1X.VL53L1xUserRoi(12, 0, 15, 3))
-    tof.start_ranging(1)
-    distance_in_mm4 = tof.get_distance()
-    tof.stop_ranging()
 
-    tof.set_user_roi(VL53L1X.VL53L1xUserRoi(0, 4, 3, 7))
-    tof.start_ranging(1)
-    distance_in_mm5 = tof.get_distance()
-    tof.stop_ranging()
-
-    tof.set_user_roi(VL53L1X.VL53L1xUserRoi(4, 4, 7, 7))
-    tof.start_ranging(1)
-    distance_in_mm6 = tof.get_distance()
-    tof.stop_ranging()
-
-    tof.set_user_roi(VL53L1X.VL53L1xUserRoi(8, 4, 11, 7))
-    tof.start_ranging(1)
-    distance_in_mm7 = tof.get_distance()
-    tof.stop_ranging()
-
-    print(str(distance_in_mm1) + " " + str(distance_in_mm2)+ " " + str(distance_in_mm3)+ " " + str(distance_in_mm4)+ " " + str(distance_in_mm5))
-
-    #print("Distance: {}mm".format(distance_in_mm))
-    #time.sleep(0.1)
+anim = animation.FuncAnimation(fig, animate, init_func=init)
+plt.show()
