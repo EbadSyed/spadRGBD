da40f63774bb49bf11720cd03d6e3c8596b64140
EbadSyed <syedebadurrahman@gmail.com>
2021-03-18 16:20:47 -0400

    qwicc lib user direct

--- readVL53.py
+++ readVL53.py
@@ -2,61 +2,54 @@ import time
 import sys
 import signal
 
-import VL53L1X
+import qwiic_vl53l1x
 
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 # Open and start the VL53L1X sensor.
-tof = VL53L1X.VL53L1X(i2c_bus=1, i2c_address=0x29)
-tof.open()
-tof.start_ranging(3)
+tof = qwiic_vl53l1x.QwiicVL53L1X()
 
-running = True
+if tof.sensor_init() is None:  # Begin returns 0 on a good init
+    print("Sensor online!\n")
 
 
 def exit_handler(signal, frame):
-    global running
-    running = False
     tof.stop_ranging()
-    print()
+    print("Exiting Now")
     sys.exit(0)
 
 
 # Attach a signal handler to catch SIGINT (Ctrl+C) and exit gracefully
 signal.signal(signal.SIGINT, exit_handler)
 
-roi = []
+# Sets Distance Mode Short=1 Long=2
+tof.set_distance_mode(1)
 
-for y in range(4):
-    for x in range(4):
-        roi.append(VL53L1X.VL53L1xUserRoi(x*4, 15 - y*4, x*4+3, 15 - 3 - y*4))
+data = np.zeros((2, 2))
 
-
-fig = plt.figure()
-data = np.zeros((4, 4))
-im = plt.imshow(data,cmap='magma',vmin=0,vmax=2400)
-plt.colorbar(fraction=0.1, pad=0.04)
-
-
-def init():
-    im.set_data(np.zeros((4, 4)))
-
-
-def animate(iter):
-
-    i = 0
-
-    for x in range(4):
-        for y in range(4):
-            tof.set_user_roi(roi[i])
-            tof.start_ranging(0)
-            data[x,y] = tof.get_distance()
-            tof.stop_ranging()
-          
-    im.set_data(data)
-
-
-anim = animation.FuncAnimation(fig, animate, init_func=init)
-plt.show()
+# fig = plt.figure()
+# im = plt.imshow(data, cmap='magma', vmin=0, vmax=2400)
+# plt.colorbar(fraction=0.1, pad=0.04)
+#
+#
+# def init():
+#     im.set_data(np.zeros((4, 4)))
+#
+#
+# def animate(iter):
+#     i = 0
+#
+#     for x in range(4):
+#         for y in range(4):
+#             tof.set_user_roi(roi[i])
+#             tof.start_ranging(0)
+#             data[x, y] = tof.get_distance()
+#             tof.stop_ranging()
+#
+#     im.set_data(data)
+#
+#
+# anim = animation.FuncAnimation(fig, animate, init_func=init)
+# plt.show()
