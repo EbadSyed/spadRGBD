4662274e5172a035eecbf0cef1c316f706daf416
Ebad Syed <syedebadurrahman@gmail.com>
2021-03-18 07:33:18 -0400

    reduce roi

--- readVL53.py
+++ readVL53.py
@@ -11,6 +11,7 @@ import matplotlib.animation as animation
 # Open and start the VL53L1X sensor.
 tof = VL53L1X.VL53L1X(i2c_bus=1, i2c_address=0x29)
 tof.open()
+tof.start_ranging(3)
 
 running = True
 
@@ -28,18 +29,17 @@ signal.signal(signal.SIGINT, exit_handler)
 
 roi = []
 
-for x1 in range(5):
-    for y1 in range(5):
-        roi.append(VL53L1X.VL53L1xUserRoi((3 * x1), (15 - 3 * y1), (3 * x1 + 3), (15 - 3 * y1 - 3)))
+
+roi.append(VL53L1X.VL53L1xUserRoi((3 * x1), (15 - 3 * y1), (3 * x1 + 3), (15 - 3 * y1 - 3)))
 
 fig = plt.figure()
 data = np.zeros((5, 5))
-im = plt.imshow(data, cmap='magma', vmin=0, vmax=500)
+im = plt.imshow(data,cmap='magma',vmin=0,vmax=2400)
 plt.colorbar(fraction=0.1, pad=0.04)
 
 
 def init():
-    im.set_data(np.zeros((8, 8)))
+    im.set_data(np.zeros((5, 5)))
 
 
 def animate(iter):
@@ -49,10 +49,10 @@ def animate(iter):
     for x in range(5):
         for y in range(5):
             tof.set_user_roi(roi[i])
-            tof.start_ranging(1)
+            tof.start_ranging(0)
             data[x,y] = tof.get_distance()
             tof.stop_ranging()
-
+          
     im.set_data(data)
 
 
